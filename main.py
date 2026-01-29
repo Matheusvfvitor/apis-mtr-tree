81afb311632f825f57d8f5ac731577a9e2ad6a98
Matheusvfvitor <34576014+Matheusvfvitor@users.noreply.github.com>
2026-01-28 22:48:26 -0300

    Remove IMA manifesto request class and function
    Removed ConsultaIMAManifestoRequest class and its related function consultar_manifesto_ima from main.py.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from services.fepam import (ConsultaFepamManifestoRequest, retorna_manifesto_fepam)
 from services.feam import (ConsultaMTRRequest, gerar_token_feam, consultar_manifesto)
+from services.ima import(ConsultaIMAManifestoRequest,retorna_manifesto_ima)
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -42,38 +43,6 @@ def buscar_mtr(dados: ConsultaMTRRequest):
         )
 
 
-class ConsultaIMAManifestoRequest(BaseModel):
-    cpf: str
-    cnpj: str
-    senha: str
-    unidadeGerador: str
-    codigoBarras: str
-
-
-def consultar_manifesto_ima(
-    codigo_barras: str,
-    unidade_gerador: str,
-    senha: str,
-    cnpj: str
-):
-    url = (
-        f"https://mtr.ima.sc.gov.br/mtrservice/retornaManifesto/"
-        f"{codigo_barras}/{unidade_gerador}/{senha}/{cnpj}"
-    )
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    response = requests.post(url, headers=headers, timeout=30)
-
-    if response.status_code != 200:
-        raise HTTPException(
-            status_code=502,
-            detail="Erro ao consultar manifesto no IMA"
-        )
-
-    return response.json()
 
 @app.post("/ima/retorna-manifesto-codigo-de-barras")
 def retorna_manifesto_ima(dados: ConsultaIMAManifestoRequest):
